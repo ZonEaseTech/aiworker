1664b9785dbdbaf3c8b469f7e4f5dbe078c726dd
BenDaye <ben@ttpos.com>
2026-05-11 12:19:14 +0800

    refactor: 拆分 Worker Web 样式模块

--- apps/web/src/app/main.tsx
+++ apps/web/src/app/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { WorkerStudio } from '../worker/worker-studio'
-import '../worker/studio.css'
+import '../styles/index.css'
 
 const rootElement = document.getElementById('root')
 if (!rootElement)
