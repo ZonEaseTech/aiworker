2585ee04f6232f9746ba4a3478c413fa98e056f4
BenDaye <yypsgdsg@163.com>
2026-05-28 20:14:39 +0800

    fix(ui): 拆分按钮组样式变体

--- packages/ui/src/components/button-group.tsx
+++ packages/ui/src/components/button-group.tsx
@@ -2,25 +2,8 @@ import type { VariantProps } from 'class-variance-authority'
 import { Separator } from '#components/separator'
 
 import { cn } from '#lib/utils'
-import { cva } from 'class-variance-authority'
 import { Slot } from 'radix-ui'
-
-const buttonGroupVariants = cva(
-  'group/button-group flex w-fit items-stretch *:focus-visible:relative *:focus-visible:z-10 has-[>[data-slot=button-group]]:gap-2 has-[select[aria-hidden=true]:last-child]:[&>[data-slot=select-trigger]:last-of-type]:rounded-r-md [&>[data-slot=select-trigger]:not([class*=\'w-\'])]:w-fit [&>input]:flex-1',
-  {
-    variants: {
-      orientation: {
-        horizontal:
-          '[&>*:not(:first-child)]:rounded-l-none [&>*:not(:first-child)]:border-l-0 [&>*:not(:last-child)]:rounded-r-none [&>[data-slot]:not(:has(~[data-slot]))]:rounded-r-md!',
-        vertical:
-          'flex-col [&>*:not(:first-child)]:rounded-t-none [&>*:not(:first-child)]:border-t-0 [&>*:not(:last-child)]:rounded-b-none [&>[data-slot]:not(:has(~[data-slot]))]:rounded-b-md!',
-      },
-    },
-    defaultVariants: {
-      orientation: 'horizontal',
-    },
-  },
-)
+import { buttonGroupVariants } from './button-group-variants'
 
 function ButtonGroup({
   className,
@@ -80,5 +63,4 @@ export {
   ButtonGroup,
   ButtonGroupSeparator,
   ButtonGroupText,
-  buttonGroupVariants,
 }
